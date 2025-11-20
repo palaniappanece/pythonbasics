c336dfb2d79ea8889bf61b94e356362fb839fa33
palaniappanece <palaniappan.thillainathan@gmail.com>
2025-11-20 11:19:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,9 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
     
 #localhost:5000/home
+
     
